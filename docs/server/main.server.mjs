04e3fc06e2a283176f62b5332b48e453493a4b68
Oluwaseni Ajibade <Ajibade.Oluwaseni@chevron.com>
2025-10-31 23:16:02 +0100

    Created docs, moved folders into docs

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-E3ZY6GNB.mjs";import"./chunk-GUQWHW2M.mjs";import"./chunk-ZGSADFKD.mjs";import"./chunk-2BCQ47VD.mjs";import"./chunk-AH762YS3.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
--- docs/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as o,c as r,d as n}from"./chunk-GUQWHW2M.mjs";import"./chunk-2BCQ47VD.mjs";import{Sa as e}from"./chunk-AH762YS3.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
