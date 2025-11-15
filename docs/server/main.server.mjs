38415a6a734d60f0a395848613e42562517d2056
Oluwaseni Ajibade <Ajibade.Oluwaseni@chevron.com>
2025-11-15 20:49:32 +0100

    Reduced Sizes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UQ7RDVQK.mjs";import"./chunk-GUQWHW2M.mjs";import"./chunk-ZGSADFKD.mjs";import"./chunk-2BCQ47VD.mjs";import"./chunk-AH762YS3.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
+import{a}from"./chunk-KNHFQMES.mjs";import"./chunk-GUQWHW2M.mjs";import"./chunk-ZGSADFKD.mjs";import"./chunk-2BCQ47VD.mjs";import"./chunk-AH762YS3.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
