a3e7bde6cb734c804a97933f32c6dcedf5a0e68f
Oluwaseni Ajibade <Ajibade.Oluwaseni@chevron.com>
2025-11-15 21:32:15 +0100

    Include the maps image

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JBQK4OXC.mjs";import"./chunk-GUQWHW2M.mjs";import"./chunk-ZGSADFKD.mjs";import"./chunk-2BCQ47VD.mjs";import"./chunk-AH762YS3.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
+import{a}from"./chunk-NXZYJFA6.mjs";import"./chunk-GUQWHW2M.mjs";import"./chunk-ZGSADFKD.mjs";import"./chunk-2BCQ47VD.mjs";import"./chunk-AH762YS3.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
